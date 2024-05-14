27aea06783413df991afb9c72a8d57b2af61e5f9
DavidK1412 <davidcasallas013@gmail.com>
2024-05-13 21:20:43 -0500

    backend mods

--- backend/authApp/views/userCreateView.py
+++ backend/authApp/views/userCreateView.py
@@ -28,3 +28,13 @@ class UserCreateView(views.APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, *args, **kwargs):
+        if not validate_admin(request.META.get('HTTP_AUTHORIZATION')[7:]):
+            return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+        user = User.objects.get(pk=kwargs['pk'])
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
